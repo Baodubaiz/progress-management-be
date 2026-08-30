1e35135b460eb4acdf92bd8181a865242dd2348b
Baodubaiz <baolam12122004@gmail.com>
2026-08-30 20:51:21 +0700

    refactor: remove vercel config files and prepare for Render deployment

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-// Vercel serverless entry point
-// This file exports the Express app for Vercel's @vercel/node handler
-// Local dev still uses src/server.ts (with app.listen)
-
-import app from '../src/app';
-
-export default app;
-
